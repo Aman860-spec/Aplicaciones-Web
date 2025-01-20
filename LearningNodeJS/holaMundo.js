5e6667f4ea5d975ec15d8a55cd9e663907cf5ecf
Abdiel <abdiel1mendezaux@gmail.com>
2025-01-20 13:39:25 -0600

    hello world

--- LearningNodeJS/holaMundo.js
+++ LearningNodeJS/holaMundo.js
@@ -1,7 +1,1 @@
-var nom;
-process.stdout.write("¿cuál es tu nombre? \n ");
-process.stdin.on('data', function(data){
-    nom = data.toString().trim();
-    process.stdout.write("hola " + nom +", bienvenido a nodejs \n");
-    process.exit();
-});
+console.log("Hello World")
